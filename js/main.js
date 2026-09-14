b1033b6f9d408b7ec4ba18f773769f5eaacb3019
최현준 <gusrb89838983@c5r6s7.codyssey.kr>
2026-09-14 17:07:01 +0900

    feat: 다크모드 localstorage 저장/복원, nav 부드러운 스크 구현

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'dark') {
+    document.body.setAttribute('data-theme', 'dark');
+}
 const hamburgerBtn = document.querySelector('#hamburger-btn');
 const nav = document.querySelector('nav');
 const darkModeBtn = document.querySelector('#dark-mode-btn');
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const targetId = link.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
 
 hamburgerBtn.addEventListener('click', () => {
     nav.classList.toggle('active');
@@ -8,10 +24,14 @@ hamburgerBtn.addEventListener('click', () => {
 
 darkModeBtn.addEventListener('click', () => {
     const currentTheme = document.body.getAttribute('data-theme');
+    let newTheme;
 
     if (currentTheme === 'dark') {
-        document.body.setAttribute('data-theme', 'light');
+        newTheme = 'light';
     } else {
-        document.body.setAttribute('data-theme', 'dark');
+        newTheme = 'dark';
     }
+
+    document.body.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
 });
